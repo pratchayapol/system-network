ea3532bf639b3dae5af37248e38d84887944224a
pratchayapol <pratchayapol2543@gmail.com>
2024-11-05 01:14:07 +0700

    cx

--- src/home.php
+++ src/home.php
@@ -112,7 +112,7 @@ $thai_months = [
                         confirmButtonText: 'ตกลง'
                     }).then((result) => {
                         if (result.isConfirmed) {
-                            window.location = /home;
+                            window.location.href = '/home'; // ไปที่หน้า /home เมื่อกดตกลง
                         }
                     });
                   </script></div>";
@@ -125,7 +125,7 @@ $thai_months = [
                         confirmButtonText: 'ตกลง'
                     }).then((result) => {
                         if (result.isConfirmed) {
-                            window.location = /home;
+                            window.location.href = '/home'; // ไปที่หน้า /home เมื่อกดตกลง
                         }
                     });
                   </script></div>";
@@ -139,7 +139,7 @@ $thai_months = [
                     confirmButtonText: 'ตกลง'
                 }).then((result) => {
                     if (result.isConfirmed) {
-                        window.location = /home;
+                        window.location.href = '/home'; // ไปที่หน้า /home เมื่อกดตกลง
                     }
                 });
               </script></div>";
@@ -154,7 +154,7 @@ $thai_months = [
                     confirmButtonText: 'ตกลง'
                 }).then((result) => {
                     if (result.isConfirmed) {
-                        window.location = /home;
+                        window.location.href = '/home'; // ไปที่หน้า /home เมื่อกดตกลง
                     }
                 });
               </script></div>";
