20e9a7aa1135347e3363f226e48ff041c7089c13
pratchayapol <pratchayapol2543@gmail.com>
2024-12-04 20:20:50 +0700

    cxxx

--- src/home.php
+++ src/home.php
@@ -239,7 +239,7 @@ $thai_months = [
                     <hr class="border-dashed border-2 border-gray-300 my-4">
                     <p class="mt-4 text-gray-700"> QR CODE </p>
                     <center>
-                        <img src="QRC_K+.jpg" alt="" class="cursor-pointer" onclick="openModal123(this.src)" width="350px">
+                        <img src="IMG_5551.JPEG" alt="" class="cursor-pointer" onclick="openModal123(this.src)" width="350px">
                     </center>
 
                     <!-- Modal -->
