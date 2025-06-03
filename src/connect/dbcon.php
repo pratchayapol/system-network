b3574c213127b21a5a29a5d8bf3c8cefcdd12e64
pratchayapol <pratchayapol2543@gmail.com>
2025-06-03 16:07:41 +0700

    cx

---
+++ src/connect/dbcon.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "mariadb-project";
+$port = 3341;
+$username = "root";
+$password = "adminpcn";
+$dbname = "system_network";
+
+try {
+    $dsn = "mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+
+    // ตั้งค่า PDO ให้แสดงข้อผิดพลาดแบบ exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //echo "เชื่อมต่อฐานข้อมูลสำเร็จ!";
+} catch (PDOException $e) {
+    echo "เชื่อมต่อฐานข้อมูลล้มเหลว: " . $e->getMessage();
+}
+?>
